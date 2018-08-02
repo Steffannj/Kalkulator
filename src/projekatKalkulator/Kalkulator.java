7f82f07870d81162db92025753703bf4bfd0225e
Elmin Hodzic <elmin.hodzic@hotmail.com>
2018-08-02 11:45:46 +0200

    Implementirati kalkulator

---
+++ src/projekatKalkulator/Kalkulator.java
@@ -0,0 +1,19 @@
+package projekatKalkulator;
+
+public class Kalkulator {
+	public int sabiranje(int a, int b) {
+
+	}
+
+	public int oduzimanje(int a, int b) {
+
+	}
+
+	public int mnozenje(int a, int b) {
+
+	}
+
+	public int dijeljenje(int a, int b) {
+
+	}
+}
